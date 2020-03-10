2b4a289bccb00bb0cae40a3f0dee554d4bfee518
Abhishek Jha <abhishekjha3012@gmail.com>
2020-03-10 20:44:18 +0530

    logout functionality
    1. header implemented with logout functionality
    2. removed unused components

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { ReactComponent as LogoutIcon } from '../../assets/logout.svg'
+
+const useStyles = makeStyles(theme => ({
+	header:{
+		display: 'grid',
+		gridTemplateColumns: '1fr 25px',
+		justifyContent: 'center'
+	},
+}));
+
+export default function Header() {
+
+	const classes = useStyles();
+	const history = useHistory();
+
+	const triggerLogout = () => {
+		localStorage.removeItem('username');
+		localStorage.removeItem('password');
+		history.push("/login") ;
+	}
+
+	return (
+		<div className={classes.header}> 
+			<h2>Logo</h2> 
+			<div className={classes.logoutIcon} onClick={triggerLogout}>
+				<LogoutIcon width='100%' height='100%' />
+			</div>
+		</div>
+	);
+}
